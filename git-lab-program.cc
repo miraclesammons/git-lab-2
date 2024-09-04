9d53ed4881e42734b660b513e5ef8c3c08fe6f2d
Miracle Sammons <ms219322@ohio.edu>
2024-09-04 15:30:24 -0400

    Answer to question 13&14

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Miracle Sammons
+ *        Date: 9/4/2024
+ * Description: Second Lab Work
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
